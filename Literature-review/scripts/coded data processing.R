d2773866b58c3e63e108d6e5d505a68d5911ea7e
EnricoToffalini <enrico.toffalini@unipd.it>
2025-07-21 12:11:07 +0200

    added data processing review

---
+++ Literature-review/scripts/coded data processing.R
@@ -0,0 +1,15 @@
+
+# load libraries
+library(readxl)
+
+# import data
+df = data.frame(read_excel("../data/data_extraction.xlsx"))
+
+# coders agreement
+1 - mean(is.na(df$conclusion_agreement))
+mean(df$conclusion_agreement, na.rm=T)
+df$key = paste0(df$ID_article,df$ID_sample,df$subsample)
+x = aggregate(df$admixture_agreement, by=list(df$key), FUN=mean, na.rm=T)
+mean(x$x, na.rm=T)
+
+
